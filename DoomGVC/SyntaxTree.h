ba786dce6e7c10d402a4235f57c0fc47da8a7528
Milan Zaretsky <milanleonidovichzaretsky@gmail.com>
2024-12-02 15:05:45 +0200

    Created 2 classes for the AST, SyntaxTreeNode and SyntaxTree, made for them some methods for easier usage

--- DoomGVC/SyntaxTree.h
+++ DoomGVC/SyntaxTree.h
@@ -1,10 +1,34 @@
-#pragma once
-class SyntaxTree
+#include <vector>
+#include <string>
+#include <memory>
+
+class SyntaxTreeNode
 {
 public:
+    std::string value;
+    std::vector<std::shared_ptr<SyntaxTreeNode>> children;
 
+    SyntaxTreeNode(const std::string& val) : value(val) {}
 
+    void addChild(const std::shared_ptr<SyntaxTreeNode>& child) {
+        children.push_back(child);
+    }
+};
+
+class SyntaxTree
+{
+
+public:
+    SyntaxTree() : root(nullptr) {}
+
+    void setRoot(const std::shared_ptr<SyntaxTreeNode>& node) {
+        root = node;
+    }
+
+    std::shared_ptr<SyntaxTreeNode> getRoot() const {
+        return root;
+    }
 
 private:
-
+    std::shared_ptr<SyntaxTreeNode> root;
 };
