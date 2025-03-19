29676442ea6f181018f2f8059ad195f23e8beb19
Milan228 <milanleonidovichzaretsky@gmail.com>
2025-03-19 21:24:36 +0200

    Took a step back

--- DoomGVC/output.c
+++ DoomGVC/output.c
@@ -1,12 +1,2 @@
-int globalArray[3][3] = {
-                            {1, 2, 3},
-                            {4, 5, 6},
-                            {7, 8, 9}
-                        };
-
-int main() {
-    int localArray[2][2] = {{5, 6}, {7, 8}};
-    int x = globalArray[2][1];
-    int y = localArray[1][1];
-    return x + y;
-}
+int arr[5] = {1, 2, 3, 4, 5};
+int main() { return arr[2]; }
--- DoomGVC/test.c
+++ DoomGVC/test.c
@@ -1,12 +1,5 @@
-int globalArray[3][3] = {
-                            {1, 2, 3},
-                            {4, 5, 6},
-                            {7, 8, 9}
-                        };
-
-int main() {
-    int localArray[2][2] = {{5, 6}, {7, 8}};
-    int x = globalArray[2][1];
-    int y = localArray[1][1];
-    return x + y;
+int main()
+{
+    int arr[3] = {1, 2, 3};
+    return arr[2];
 }
